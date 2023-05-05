4510a56ce6e4412954a151658206ab0cd5ff6e1a
Richard Velasquez <revelasquez666@gmail.com>
2023-05-05 15:13:31 -0400

    serializadores de los modelos y tareas de evaluacion y encuestas

--- survey/api_survey/serializer.py
+++ survey/api_survey/serializer.py
@@ -1,19 +1,23 @@
 from rest_framework import serializers
 from .models import Employee
 from .models import Area
+from .models import Evaluation
+from .models import Survey
+from .models import Question
+from .models import Answer
+from .models import AnswerOption
 
 class EmployeeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Employee
         fields = (
-            'id',
             'first_name',
             'second_name',
             'last_name',
             'second_last_name',
             'identity_card',
             'active',
-            'area_id',
+            'area',
         )
         
 class AreaSerializer(serializers.ModelSerializer):
@@ -21,4 +25,49 @@ class AreaSerializer(serializers.ModelSerializer):
         model = Area
         fields = (
             'name',
+        )
+        
+class SurveySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Survey
+        fields = (
+            'code',
+            'description',
+        )
+        
+class EvaluationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Evaluation
+        fields = (
+            'employee',
+            'survey'
+        )
+        
+class QuestionSerialize(serializers.ModelSerializer):
+    class Meta:
+        model = Question
+        fields = (
+            'survey',
+            'code',
+            'description',
+            'state'
+        )
+        
+class AnswerSerialize(serializers.ModelSerializer):
+    class Meta:
+        model = Answer
+        fields = (
+            'evaluation',
+            'question',
+            'answer_option',
+        )
+        
+class AnswerOptionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AnswerOption
+        fields = (
+            'code',
+            'descrition',
+            'question',
+            'state',
         )
---
+++ survey/api_survey/views/survey.views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from rest_framework import generics
+from api_survey.models import Employee
+from api_survey.models import Area
+from api_survey.serializer import AreaSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+class GetArea(APIView):
+    def get(self, request, area_id):
+        try:
+            area = Area.objects.get(id=area_id)
+        except Area.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        
+        serializer = AreaSerializer(area)
+        return Response(serializer.data)
